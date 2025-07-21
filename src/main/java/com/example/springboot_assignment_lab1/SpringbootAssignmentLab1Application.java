48f522a185971d85c91693952ff7006be9fc8ba1
bryan-c.javier <bryan-c.javier@gcash.com>
2025-07-21 23:55:22 +0800

    Initial Commit

---
+++ src/main/java/com/example/springboot_assignment_lab1/SpringbootAssignmentLab1Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot_assignment_lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAssignmentLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAssignmentLab1Application.class, args);
+	}
+
+}
